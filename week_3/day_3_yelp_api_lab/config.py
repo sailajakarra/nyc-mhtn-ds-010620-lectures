5e361a653c47be507e105c5beecc0581b8b30ef0
sailajakarra <36280486+sailajakarra@users.noreply.github.com>
2020-01-26 15:57:22 -0500

    new

---
+++ week_3/day_3_yelp_api_lab/config.py
@@ -0,0 +1,9 @@
+
+
+api = { 'api_key' = '6uBv62djTro1it3HTnBN4Zg9z0A9C_NcvLK4nqIwTBWZgULES4-MkL_gUkC7SRvpPZRmjjUz3ZxNgz4ve8bx5SpRpkRBn-h7jlwbKjV7_P_MmkHaNCcTI4GDt_cpXnYx'
+        'client_id' = 'kflHoGM8gsEdTnSm9zZQ4w
+}
+my_cred = { 'host' : "database-1.c8yct8sv0zo0.us-east-2.rds.amazonaws.com",
+    'user' : "admin",
+    'passwd': 'FLATIRON4EVER',
+    'port':3306     }
